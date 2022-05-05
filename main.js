5639482d1bbd8302c7499b7b8fbf7c09a2f1dac1
Erisa Sala <erisasala7@gmail.com>
2022-05-05 16:56:30 +0200

    scope

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const subscription = async() => {
     // Service Worker
     console.log("Registering a Service worker");
     const register = await navigator.serviceWorker.register("https://erisasala7.github.io/push-pwa-ba-demo/worker.js", {
-        scope: "https://erisasala7.github.io/push-pwa-ba-demo"
+        scope: "https://erisasala7.github.io/push-pwa-ba-demo/"
     });
     console.log("New Service Worker");
 
